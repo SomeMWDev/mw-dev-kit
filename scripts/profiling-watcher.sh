915809a70bf514c912bc5554359504c5e1eee138
SomeRandomDeveloper <thisisnotmyname275@gmail.com>
2025-11-18 01:08:23 +0100

    refactor: drop profiling watcher
    Now present in mwutil, compatible with non-wayland environments and doesn't require inotify-tools anymore

--- scripts/profiling-watcher.sh
+++
@@ -1,5 +0,0 @@
-# this requires sudo dnf install inotify-tools wl-clipboard
-while inotifywait -e close_write ../core/cache/speedscope.json; do
-    wl-copy < ../core/cache/speedscope.json
-    echo "Copied updated file to clipboard at $(date)"
-done
